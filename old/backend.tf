73520ac8d039cddc0afb1dd0563b175a2f21b407
SSchneider22 <noreason417@gmail.com>
2025-01-01 12:17:21 +0900

    oldを削除

--- old/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "sagara-terraform-state-bucket"
-    key            = "snowflake-state/snowflake.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "sagara-terraform-state-lock-table"
-    encrypt        = true
-  }
-}
